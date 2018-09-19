1fb236bc51b018a9a886a873d84aefe4409ef6a0
Justin Herzog <justinherzog@justins-macbook-pro.local>
2018-09-19 08:48:22 -0600

    Finished the UIImage lab thing. (Section 2, unit 8)

---
+++ Unit 2 lesson 8 lab/Unit 2 lesson 8 lab/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Unit 2 lesson 8 lab
+//
+//  Created by Justin Herzog on 9/19/18.
+//  Copyright © 2018 Justin Herzog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
