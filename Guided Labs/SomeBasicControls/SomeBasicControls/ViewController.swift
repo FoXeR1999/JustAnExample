c041930c320bd250a63e21433be353bd65de6079
Justin Herzog <justinherzog@justins-macbook-pro.local>
2018-09-20 10:16:52 -0600

    Got about halfway through the lab thing

---
+++ Guided Labs/SomeBasicControls/SomeBasicControls/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  SomeBasicControls
+//
+//  Created by Justin Herzog on 9/20/18.
+//  Copyright © 2018 Justin Herzog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    // MARK: This is the Switch
+    
+    @IBOutlet weak var toggledChangeBackground: UISwitch!
+    
+    @IBAction func changeBackground(_ sender: Any) {
+        if toggledChangeBackground.isOn {
+            self.view.backgroundColor = UIColor.white
+        } else {
+            self.view.backgroundColor = UIColor(red: 0.5569, green: 0.5569, blue: 0.5569, alpha: 1.0)
+        }
+        
+    }
+    
+    // MARK: This is the Slider
+    
+    @IBOutlet weak var toggledChangeHue: UISlider!
+    
+    
+    @IBAction func changeHue(_ sender: Any) {
+        var sliderValue = toggledChangeHue.value
+        print(toggledChangeHue.value)
+        if (toggledChangeHue != nil) {
+            self.view.backgroundColor = UIColor(hue: CGFloat(sliderValue), saturation: 0.5, brightness: 1, alpha: 1.0)
+        }
+        
+    }
+}
