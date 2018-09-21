2dc0584c94521d068e9cbf66f85fa6ca83ef8639
Justin Herzog <justinherzog@justins-macbook-pro.local>
2018-09-21 09:43:30 -0600

    Finished the calculator gui

---
+++ Guided Labs/Calculator/Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Justin Herzog on 9/21/18.
+//  Copyright © 2018 Justin Herzog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
