a8c913563215fe7f8cf0973206b7afebf890993c
Justin Herzog <justinherzog@justins-macbook-pro.local>
2018-09-24 08:06:32 -0600

    Finished the structures lab

--- 2 - Introduction to UIKit/3 - Structures/lab/Lab - Structures.playground/Pages/10. App Exercise - Type Properties and Methods.xcplaygroundpage/Contents.swift
+++ 2 - Introduction to UIKit/3 - Structures/lab/Lab - Structures.playground/Pages/10. App Exercise - Type Properties and Methods.xcplaygroundpage/Contents.swift
@@ -14,14 +14,20 @@ struct RunningWorkout {
     var time: Double
     var elevation: Double
     
-    static var mileTimeFor(distance:time:) -> Double {
-        
+    static func mileTimeFor(distance: Double, time: Double) -> Double {
+        return time / distance * 1600
     }
+    
+    static var meterInFeet = 3.28084
+    static var mileInMeters = 1600.0
 }
+
+print(RunningWorkout.mileTimeFor(distance: 7895, time: 64))
 /*:
  It may be helpful to have a few type properties on `RunningWorkout` representing unit conversions (i.e. meters to mile, feet to meters, etc.). Go back and add a type property for `meterInFeet` and assign it 3.28084. Then add a type property for `mileInMeters` and assign it 1600.0. Print both of these values below.
  */
-
+print(RunningWorkout.meterInFeet)
+print(RunningWorkout.mileInMeters)
 
 /*:
  
--- 2 - Introduction to UIKit/3 - Structures/lab/Lab - Structures.playground/Pages/9. Exercise - Type Properties and Methods.xcplaygroundpage/Contents.swift
+++ 2 - Introduction to UIKit/3 - Structures/lab/Lab - Structures.playground/Pages/9. Exercise - Type Properties and Methods.xcplaygroundpage/Contents.swift
@@ -8,30 +8,30 @@ struct User {
     var email: String
     var age: Int
     
-    static var currentUser = User(userName: "J-ZAWG", email: "justinzog@gmail.com", age: 17)
+    static var currentUser: User = User(userName: "J-ZAWG", email: "justinzog@gmail.com", age: 17)
     
-    static func logIn(user: User) {
-        currentUser = user
-       // print("\(userName) has joined the party")
+    static func logIn(currentLoggedInUser: User) {
+        currentUser = currentLoggedInUser
+        print("\(currentUser.userName) has joined the party")
     }
 }
 
-
+print(User.currentUser)
 /*:
  There are other properties and actions associated with a `User` struct that might be good candidates for a type property or method. One might be a method for logging in. Go back and create a type method called `logIn(user:)` where `user` is of type `User`. In the body of the method, assign the passed in user to the `currentUser` property, and print out a statement using the user's userName saying that the user has logged in.
  
  Below, call the `logIn(user:)` method and pass in a different `User` instance than what you assigned to currentUser above. Observe the printout in the console.
  */
-
-
+let Suzashi = User(userName: "Suzashi", email: "justinzog@gmail.com", age: 17)
+User.logIn(currentLoggedInUser: Suzashi)
 //: [Previous](@previous)  |  page 9 of 10  |  [Next: App Exercise - Type Properties and Methods](@next)
-struct Cake {
-    var color: String
-    var flavor: String
-}
-
-let justinCake = Cake(color: "Yellow", flavor: "Vanilla")
-
-var color = justinCake.color
-
-print(color)
+//struct Cake {
+//    var color: String
+//    var flavor: String
+//}
+//
+//let justinCake = Cake(color: "Yellow", flavor: "Vanilla")
+//
+//var color = justinCake.color
+//
+//print(color)
