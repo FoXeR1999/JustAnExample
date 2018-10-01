2172c65791d45e03f1968f6548cb43acb0b617ab
Justin Herzog <justinherzog@xx-memedealer-xx.local>
2018-10-01 09:46:55 -0600

    Almost done with the Guard lab

--- 3 - Navigation and Workflows/3 - Guard/lab/Lab - Guard.playground/Pages/1. Exercise - Guard Statements.xcplaygroundpage/Contents.swift
+++ 3 - Navigation and Workflows/3 - Guard/lab/Lab - Guard.playground/Pages/1. Exercise - Guard Statements.xcplaygroundpage/Contents.swift
@@ -4,13 +4,44 @@ import UIKit
 
  Imagine you want to write a function to calculate the area of a rectangle. However, if you pass a negative number into the function, you don't want it to calculate a negative area. Create a function called `calculateArea` that takes two `Double` parameters, `x` and `y`, and returns an optional `Double`. Write a guard statement at the beginning of the function that verifies each of the parameters is greater than zero and returns `nil` if not. When the guard has succeeded, calculate the area by multiplying `x` and `y` together, then return the area. Call the function once with positive numbers and once with at least one negative number.
 */
+func calculateArea(x: Double, y: Double) -> Double? {
+    guard x > 0.0 else {
+        print("x can't be a negative value.")
+        return nil
+    }
+    
+    
+    guard y > 0.0 else {
+        print("y can't be a negative value.")
+        return nil
+    }
+    
+    return Double(x * y)
+}
 
+print(calculateArea(x: 5, y: 3))
 
+if let unwrappedCalculatedArea = calculateArea(x: 5, y: 3) {
+    print(unwrappedCalculatedArea)
+}
+
+print(calculateArea(x: -3, y: 5))
 /*:
  Create a function called `add` that takes two optional integers as parameters and returns an optional integer. You should use one `guard` statement to unwrap both optional parameters, returning `nil` in the `guard` body if one or both of the parameters doesn't have a value. If both parameters can successfully be unwrapped, return their sum. Call the function once with non-`nil` numbers and once with at least one parameter being `nil`.
  */
+func add(x: Int?, y: Int?) -> Int? {
+    guard let unwrappedX = x, let unwrappedY = y else {
+        return nil
+    }
+    
+    return (unwrappedX + unwrappedY)
+}
 
+if let unwrappedAdd = add(x: 5, y: 18) {
+    print(unwrappedAdd)
+}
 
+print(add(x: nil, y: 15))
 /*:
  When working with UIKit objects, you will occasionally need to unwrap optionals to handle user input. For example, the text fields initialized below have `text` properties that are of type `String?`. 
  
@@ -30,10 +61,38 @@ firstNameTextField.text = "Jonathan"
 lastNameTextField.text = "Sanders"
 ageTextField.text = "28"
 
-
+func createUser() -> User? {
+    guard let unwrappedFirstNameTextField = firstNameTextField.text, let unwrappedLastNameTextField = lastNameTextField.text, let unwrappedAgeTextField = ageTextField.text else {
+        return nil
+    }
+    
+    return User(firstName: unwrappedFirstNameTextField, lastName: unwrappedLastNameTextField, age: unwrappedAgeTextField)
+}
 /*:
  Call the function you made above and capture the return value. Unwrap the `User` with standard optional binding and print a statement using each of its properties. 
  */
-
+if let myUser = createUser() {
+    print("\(myUser.firstName) \(myUser.lastName). Age: \(myUser.age)")
+}
 
 //: page 1 of 2  |  [Next: App Exercise - Guard](@next)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
