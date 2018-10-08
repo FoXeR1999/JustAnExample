61fe9e2e68d9f06424f819820d4cc66e84507c74
Justin Herzog <justinherzog@xx-memedealer-xx.local>
2018-10-08 08:33:48 -0600

    Made the view controllers that's about it.

---
+++ Guided Labs/AboutMe/AboutMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Justin Herzog on 10/8/18.
+//  Copyright © 2018 Justin Herzog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
