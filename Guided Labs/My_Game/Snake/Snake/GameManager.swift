df2ffd19567824c644887599545e7e07aeb8cd55
Justin Herzog <justinherzog@xx-memedealer-xx.local>
2018-10-10 10:09:43 -0600

    I got to creating a game instance in the tutorial. I'm finding stuff out along the way.

---
+++ Guided Labs/My_Game/Snake/Snake/GameManager.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  Snake
+//
+//  Created by Justin Herzog on 10/10/18.
+//  Copyright © 2018 Justin Herzog. All rights reserved.
+//
+
+import SpriteKit
+
+class GameManager {
+    
+    
+    
+}
