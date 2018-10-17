0882f0c012743111153394d0331447212ff7edcc
Justin Herzog <justinherzog@xx-memedealer-xx.local>
2018-10-17 09:12:41 -0600

    Finished the lesson thing

---
+++ Guided Labs/Example with Ryan/Example with Ryan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example with Ryan
+//
+//  Created by Justin Herzog on 10/17/18.
+//  Copyright © 2018 Justin Herzog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
