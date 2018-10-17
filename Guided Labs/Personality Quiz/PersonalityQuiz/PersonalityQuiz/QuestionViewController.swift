fad2ac5e513a5a3f5d34bedadefc289d5e36d1aa
Justin Herzog <justinherzog@xx-memedealer-xx.local>
2018-10-17 10:05:26 -0600

    I have a good layout for iPhones but on the iPad it looks a bit sketchy. I'm about to mess with lots of constraints so if I eff things up pull this version from the cloud.

---
+++ Guided Labs/Personality Quiz/PersonalityQuiz/PersonalityQuiz/QuestionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  QuestionViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Justin Herzog on 10/17/18.
+//  Copyright © 2018 Justin Herzog. All rights reserved.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
