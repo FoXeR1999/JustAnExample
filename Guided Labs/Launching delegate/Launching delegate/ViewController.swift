bf7f1403d03dd23bf96a9f79914a24cddd9c785f
Justin Herzog <justinherzog@xx-memedealer-xx.local>
2018-10-23 08:48:26 -0600

    Went over the app life cycle

---
+++ Guided Labs/Launching delegate/Launching delegate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Launching delegate
+//
+//  Created by Justin Herzog on 10/23/18.
+//  Copyright © 2018 Justin Herzog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
