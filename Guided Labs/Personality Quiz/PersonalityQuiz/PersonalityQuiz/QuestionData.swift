fe1d8518afb2f0f30fa3cbf1ebfb86e279238de6
Justin Herzog <justinherzog@xx-memedealer-xx.local>
2018-10-25 08:50:10 -0600

    May have made my Enum into a UIImage??? Possibly if not pull from the commit before this one.

--- Guided Labs/Personality Quiz/PersonalityQuiz/PersonalityQuiz/QuestionData.swift
+++ Guided Labs/Personality Quiz/PersonalityQuiz/PersonalityQuiz/QuestionData.swift
@@ -7,12 +7,13 @@
 //
 
 import Foundation
+import UIKit
 
-//SkullKid = "Skull Kid", Twinrova = "Twinrova", Ghirahim = "Ghirahim", Zant = "Zant", Midna = "Midna", Fi = "Fi", Groose = "Groose", HappyMaskSalesman = "Happy Mask Salesman", Linebeck = "Linebeck", Navi = "Navi", Tingle = "Tingle, "Agitha = "Agitha", GuruGuru = "Guru-Guru", Malon = "Malon", Postman = "Postman"
+//SkullKid = "Skull Kid", Twinrova = "Twinrova", Ghirahim = "Ghirahim", Zant = "Zant", Midna = "Midna", Fi = "Fi", Groose = "Groose", HappyMaskSalesman = "Happy Mask Salesman", Linebeck = "Linebeck", Navi = "Navi", Tingle = "Tingle, "Agitha = "Agitha", GuruGuru = "Guru-Guru", Malon = "Malon", Postman = "Postman", KingDorephan = "King Dorephan", Muzu = "Muzu", Riju = "Riju", Yunobo = "Yunobo", Bolson = "Bolson",  GreatDekuTree = "Great Deku Tree", Pikango = "Pikango", Magda = "Magda", Purah = "Purah", Paya = "Paya", Robbie = "Robbie", Kass = "Kass", Hestu = "Hestu", Teba = "Teba",
 
 struct Answer {
     var text: String
-    var type: ZeldaType
+    var type: UIImage.ZeldaType
 }
 
 struct Question {
@@ -25,67 +26,39 @@ enum ResponseType {
     case single, multiple, ranged
 }
 
-enum ZeldaType: String {
-    case Link = "Link", Zelda = "Zelda", Ganon = "Ganon", MasterKohga = "Master Kohga", Kilton = "Kilton", Pikango = "Pikango", Magda = "Magda", Purah = "Purah", Paya = "Paya", Robbie = "Robbie", Kass = "Kass", Hestu = "Hestu", Teba = "Teba", Sidon = "Sidon", KingDorephan = "King Dorephan", Muzu = "Muzu", Riju = "Riju", Yunobo = "Yunobo", Bolson = "Bolson",  GreatDekuTree = "Great Deku Tree",  Impa = "Impa", KingOfRedLions = "King of Red Lions", Mipha = "Mipha", Daruk = "Daruk", Revali = "Revali", Urbosa = "Urbosa", Beedle = "Beedle",  GreatFairy = "Great Fairy"
-
-    var definition: String {
-        switch self {
-        case.Link:
-            return "Describe why they're Link."
-        case.Zelda:
-            return "Describe why they're Zelda."
-        case.Ganon:
-            return "Describe why they're Ganon."
-        case.MasterKohga:
-            return "Describe why they're Master Kohga."
-        case.Kilton:
-            return "Describe why they're Kilton."
-        case.Pikango:
-            return "Describe why they're Pikango."
-        case.Magda:
-            return "Describe why they're Magdo."
-        case.Purah:
-            return "Describe why they're Purah."
-        case.Paya:
-            return "Describe why they're Paya."
-        case.Robbie:
-            return "Describe why they're Robbie."
-        case.Kass:
-            return "Describe why they're Kass."
-        case.Hestu:
-            return "Describe why they're Hestu."
-        case.Teba:
-            return "Describe why they're Teba."
-        case.Sidon:
-            return "Describe why they're Sidon."
-        case.KingDorephan:
-            return "Describe why they're King Dorephan."
-        case.Muzu:
-            return "Describe why they're Muzu."
-        case.Riju:
-            return "Describe why they're Riju."
-        case.Yunobo:
-            return "Describe why they're Yunobo."
-        case.Bolson:
-            return "Describe why they're Bolson."
-        case.GreatDekuTree:
-            return "Describe why they're Great Deku Tree."
-        case.Impa:
-            return "Describe why they're Impa."
-        case.KingOfRedLions:
-            return "Describe why they're the Kind of Red Lions."
-        case.Mipha:
-            return "Describe why they're Mipha."
-        case.Daruk:
-            return "Describe why they're Daruk."
-        case.Revali:
-            return "Describe why they're Revali."
-        case.Urbosa:
-            return "Describe why they're Urbosa."
-        case.Beedle:
-            return "Describe why they're Beedle."
-        case.GreatFairy:
-            return "Describe why they're a Great Fairy."
+extension UIImage {
+    enum ZeldaType: String {
+        
+        case Link = "Link", Zelda = "Zelda", Ganon = "Ganon", Sidon = "Sidon", OldMan = "OldMan", Mipha = "Mipha", Daruk = "Daruk", Revali = "Revali", Urbosa = "Urbosa", Beedle = "Beedle"
+        
+        static let values = [Link, Zelda, Ganon, Sidon, OldMan, Mipha, Daruk, Revali, Urbosa, Beedle]
+        
+        var definition: String {
+            switch self {
+            case.Link:
+                return "Describe why they're Link."
+            case.Zelda:
+                return "Describe why they're Zelda."
+            case.Ganon:
+                return "Describe why they're Ganon."
+            case.OldMan:
+                return "Describe why they're the Kind of Red Lions."
+            case.Mipha:
+                return "Describe why they're Mipha."
+            case.Daruk:
+                return "Describe why they're Daruk."
+            case.Revali:
+                return "Describe why they're Revali."
+            case.Urbosa:
+                return "Describe why they're Urbosa."
+            case.Beedle:
+                return "Describe why they're Beedle."
+            case .Sidon:
+                return "Describe why they're Sidon"
+            }
         }
+    }
+    convenience init!(assetIdentifier: ZeldaType) {
+        self.init(named: assetIdentifier.rawValue)
     }
 }
