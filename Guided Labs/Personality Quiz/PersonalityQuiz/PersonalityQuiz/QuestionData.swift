caaf0fcd4ea9e96515ff57b05f9d96d4a7bb0010
Justin Herzog <justinherzog@xx-memedealer-xx.local>
2018-10-25 10:18:58 -0600

    I got pretty far along with the lab, just connected all the labels on the questions.

--- Guided Labs/Personality Quiz/PersonalityQuiz/PersonalityQuiz/QuestionData.swift
+++ Guided Labs/Personality Quiz/PersonalityQuiz/PersonalityQuiz/QuestionData.swift
@@ -11,6 +11,8 @@ import UIKit
 
 //SkullKid = "Skull Kid", Twinrova = "Twinrova", Ghirahim = "Ghirahim", Zant = "Zant", Midna = "Midna", Fi = "Fi", Groose = "Groose", HappyMaskSalesman = "Happy Mask Salesman", Linebeck = "Linebeck", Navi = "Navi", Tingle = "Tingle, "Agitha = "Agitha", GuruGuru = "Guru-Guru", Malon = "Malon", Postman = "Postman", KingDorephan = "King Dorephan", Muzu = "Muzu", Riju = "Riju", Yunobo = "Yunobo", Bolson = "Bolson",  GreatDekuTree = "Great Deku Tree", Pikango = "Pikango", Magda = "Magda", Purah = "Purah", Paya = "Paya", Robbie = "Robbie", Kass = "Kass", Hestu = "Hestu", Teba = "Teba",
 
+var questionIndex = 0
+
 struct Answer {
     var text: String
     var type: UIImage.ZeldaType
@@ -62,3 +64,51 @@ extension UIImage {
         self.init(named: assetIdentifier.rawValue)
     }
 }
+
+var questions: [Question] = [
+    Question(text: "Which food do you like the most?",
+             type:.single,
+             answers: [
+                Answer(text: "Anything Is Good", type: .Link),
+                Answer(text: "Spicy Steak", type: .Daruk),
+                Answer(text: "Roasted Fruit", type: .OldMan),
+                Answer(text: "Fish", type: .Revali)
+        ]),
+    Question(text: "Which activity do you enjoy the most?",
+             type: .single,
+             answers: [
+                Answer(text: "Swimming", type: .Sidon),
+                Answer(text: "Competing", type: .Urbosa),
+                Answer(text: "Reading", type: .Zelda),
+                Answer(text: "Catching Bugs", type: .Beedle)
+        ]),
+    Question(text: "How much do you like Link?",
+             type: .ranged,
+             answers: [
+                Answer(text: "I hate him", type: .Ganon),
+                Answer(text: "I love him", type: .Mipha)
+        ]),
+    Question(text: "Where would you most like to live?",
+             type: .single,
+             answers: [
+                Answer(text: "Hawaii", type: .Mipha),
+                Answer(text: "Near Mount Fuji", type: .Daruk),
+                Answer(text: "The Sahara Desert", type: .Urbosa),
+                Answer(text: "The Alps", type: .Revali)
+        ]),
+    //Link Zelda Ganon Old Man
+    Question(text: "What do you do when you fail at something?",
+             type: .single,
+             answers: [
+                Answer(text: "Improvise, Adapt, Overcome", type: .Link),
+                Answer(text: "Try your best to master it", type: .Zelda),
+                Answer(text: "Encourage others to do what I can't", type: .OldMan),
+                Answer(text: "Give up and rampage", type: .Ganon)
+        ]),
+    Question(text: "Do you usually start conversations?",
+             type: .ranged,
+             answers: [
+                Answer(text: "Nope, people ususally talk to me first", type: .Beedle),
+                Answer(text: "Yep, I almost always start them", type: .Sidon)
+        ])
+]
--- Guided Labs/Personality Quiz/PersonalityQuiz/PersonalityQuiz/QuestionViewController.swift
+++ Guided Labs/Personality Quiz/PersonalityQuiz/PersonalityQuiz/QuestionViewController.swift
@@ -10,12 +10,63 @@ import UIKit
 
 class QuestionViewController: UIViewController {
 
+    // Mark: Question Label
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet weak var questionProgressView: UIProgressView!
+    
+    // Mark: SingleStackView
+    
+    @IBOutlet weak var singleStackView: UIStackView!
+    
+    @IBOutlet weak var singleButton1: UIButton!
+    @IBOutlet weak var singleButton2: UIButton!
+    @IBOutlet weak var singleButton3: UIButton!
+    @IBOutlet weak var singleButton4: UIButton!
+    
+    // Mark: MultipleStackView
+    
+    @IBOutlet weak var multipleStackView: UIStackView!
+    
+    @IBOutlet weak var multiLabel1: UILabel!
+    @IBOutlet weak var multiLabel2: UILabel!
+    @IBOutlet weak var multiLabel3: UILabel!
+    @IBOutlet weak var multiLabel4: UILabel!
+    
+    // Mark: RangedStackView
+    
+    @IBOutlet weak var rangedStackView: UIStackView!
+    
+    @IBOutlet weak var rangedLabel1: UILabel!
+    @IBOutlet weak var rangedLabel2: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
         // Do any additional setup after loading the view.
     }
     
+    func updateUI() {
+        singleStackView.isHidden = true
+        multipleStackView.isHidden = true
+        rangedStackView.isHidden = true
+        
+        navigationItem.title = "Question #\(questionIndex+1)"
+        
+        let currentQuestion = questions[questionIndex]
+        
+        switch currentQuestion.type {
+        case .single:
+            singleStackView.isHidden = false
+        case .multiple:
+            multipleStackView.isHidden = false
+        case .ranged:
+            rangedStackView.isHidden = false
+        }
+       
+    }
 
     /*
     // MARK: - Navigation
