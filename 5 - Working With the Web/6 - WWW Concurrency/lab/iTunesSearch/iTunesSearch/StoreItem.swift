256e1f1262a73dc6c3b237f3a49de720b48a77b7
Justin Herzog <justinherzog@Xx-MemeDealer-xX.local>
2018-12-17 09:26:17 -0700

    Finished this lab, calling api's is pretty dope

---
+++ 5 - Working With the Web/6 - WWW Concurrency/lab/iTunesSearch/iTunesSearch/StoreItem.swift
@@ -0,0 +1,51 @@
+//
+//  StoreItem.swift
+//  iTunesSearch
+//
+//  Created by Justin Herzog on 12/17/18.
+//  Copyright © 2018 Caleb Hicks. All rights reserved.
+//
+
+import Foundation
+
+struct StoreItem: Codable {
+    var name: String
+    var artist: String
+    var description: String
+    var kind: String
+    var artworkURL: URL
+    
+    enum CodingKeys: String, CodingKey {
+        
+        case name = "trackName"
+        case artist = "artistName"
+        case kind = "kind"
+        case description = "description"
+        case artworkURL = "artworkUrl100"
+    }
+    
+    enum AdditionalKeys: String, CodingKey {
+        case longDescription
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        name = try values.decode(String.self, forKey: CodingKeys.name)
+        artist = try values.decode(String.self, forKey: CodingKeys.artist)
+        kind = try values.decode(String.self, forKey: CodingKeys.kind)
+        artworkURL = try values.decode(URL.self, forKey: CodingKeys.artworkURL)
+        
+        if let description = try? values.decode(String.self, forKey: CodingKeys.description) {
+            self.description = description
+        } else {
+            let additionalValues = try decoder.container(keyedBy: AdditionalKeys.self)
+            description = (try? additionalValues.decode(String.self, forKey: AdditionalKeys.longDescription)) ?? ""
+        }
+    }
+    
+}
+
+struct StoreItems: Codable {
+    let results: [StoreItem]
+}
+
--- 5 - Working With the Web/6 - WWW Concurrency/lab/iTunesSearch/iTunesSearch/StoreItemListTableViewController.swift
+++ 5 - Working With the Web/6 - WWW Concurrency/lab/iTunesSearch/iTunesSearch/StoreItemListTableViewController.swift
@@ -6,9 +6,9 @@ class StoreItemListTableViewController: UITableViewController {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var filterSegmentedControl: UISegmentedControl!
     
-    // add item controller property
+    let itemController = StoreItemController()
     
-    var items = [String]()
+    var items = [StoreItem]()
     
     let queryOptions = ["movie", "music", "software", "ebook"]
     
@@ -25,13 +25,26 @@ class StoreItemListTableViewController: UITableViewController {
         let searchTerm = searchBar.text ?? ""
         let mediaType = queryOptions[filterSegmentedControl.selectedSegmentIndex]
         
+        
         if !searchTerm.isEmpty {
             
-            // set up query dictionary
+            let query = [
+                "term": searchTerm,
+                "lang": "en_us",
+                "media": mediaType
+            ]
             
-            // use the item controller to fetch items
-            // if successful, use the main queue to set self.items and reload the table view
-            // otherwise, print an error to the console
+            itemController.fetchItems(matching: query, completion: { (items) in
+                
+                DispatchQueue.main.async {
+                    if let items = items {
+                        self.items = items
+                        self.tableView.reloadData()
+                    } else {
+                        print("Unable to load data.")
+                    }
+                }
+            })
         }
     }
     
@@ -39,15 +52,25 @@ class StoreItemListTableViewController: UITableViewController {
         
         let item = items[indexPath.row]
         
-        cell.textLabel?.text = item
+        cell.textLabel?.text = item.name
+        cell.detailTextLabel?.text = item.artist
+        cell.imageView?.image = UIImage(named: "gray")
         
-        // set label to the item's name
-        // set detail label to the item's subtitle
-        // reset the image view to the gray image
+        let task = URLSession.shared.dataTask(with: item.artworkURL) { (data, response, error) in
+            
+            guard let imageData = data else {
+                return
+            }
+            
+            DispatchQueue.main.async {
+                
+                let image = UIImage(data: imageData)
+                cell.imageView?.image = image
+                
+            }
+        }
         
-        // initialize a network task to fetch the item's artwork
-        // if successful, use the main queue capture the cell, to initialize a UIImage, and set the cell's image view's image to the 
-        // resume the task
+        task.resume()
     }
     
     @IBAction func filterOptionUpdated(_ sender: UISegmentedControl) {
@@ -56,17 +79,17 @@ class StoreItemListTableViewController: UITableViewController {
     }
     
     // MARK: - Table view data source
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-
+        
         return items.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "itemCell", for: indexPath)
         configure(cell: cell, forItemAt: indexPath)
-
+        
         return cell
     }
     
@@ -79,10 +102,11 @@ class StoreItemListTableViewController: UITableViewController {
 }
 
 extension StoreItemListTableViewController: UISearchBarDelegate {
-
+    
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         
         fetchMatchingItems()
         searchBar.resignFirstResponder()
     }
+    
 }
