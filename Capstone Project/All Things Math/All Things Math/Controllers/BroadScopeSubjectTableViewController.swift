5a0ff101db657d3c965887eb283f9f946e3d4723
Justin Herzog <justinherzog@Xx-MemeDealer-xX.local>
2019-02-05 09:56:49 -0700

    The data on the first 2 table views load. Now I just need a complete list Subjects and Secondary Subjects.

--- Capstone Project/All Things Math/All Things Math/Controllers/BroadScopeSubjectTableViewController.swift
+++ Capstone Project/All Things Math/All Things Math/Controllers/BroadScopeSubjectTableViewController.swift
@@ -17,78 +17,80 @@ class BroadScopeSubjectTableViewController: UITableViewController {
         
     }
     
-
+    
     // MARK: - Table view data source
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         return subjectsStruct.subjects.keys.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "SubjectCell", for: indexPath)
         
-        let subjectDictionary = (subjectsStruct.subjects.keys).sorted() // Alphabatized subjects
+        let sortedSubjectDictionary = (subjectsStruct.subjects.keys).sorted() // Alphabatized subjects
         
-        cell.textLabel?.text = subjectDictionary[indexPath.row]
-
+        cell.textLabel?.text = sortedSubjectDictionary[indexPath.row]
+        
         return cell
     }
     
-
+    
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
     /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
         guard let narrowScopeTableView = segue.destination as? NarrowScopeSubjectTableViewController else { return }
         
+        let sortedSubjectDictionary = (subjectsStruct.subjects.keys).sorted()
+        
         if let indexPath = tableView.indexPathForSelectedRow,
             segue.identifier == "toNarrowScopeTableView" {
             
+            guard let unwrappedSubject = subjectsStruct.subjects[sortedSubjectDictionary[indexPath.row]] else { return }
             
+            narrowScopeTableView.secondarySubjects = unwrappedSubject.sorted()
             
-            // get the info for the narrowScopeTVC
         }
         
     }
-    */
-
+    
 }
